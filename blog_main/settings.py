8ee36f2bd99acc94063a49e2fef8c142422a4469
Manjulmalla <mallamanjul4@gmail.com>
2026-01-05 19:27:41 +0545

    category

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e6deknl1!z-c17ec1!io=cp)3j3z_bac4^kr_$6@5%eea9^&@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blogs.context_processors.get_categories',
             ],
         },
     },
--- blog_main/views.py
+++ blog_main/views.py
@@ -8,7 +8,7 @@ def home(request):
     posts = Blog.objects.filter(is_featured=False, status='Published')
 
     context ={
-        'categories': categories,
+        
         'featured_posts' : featured_posts,
         'posts' :posts,
     }
