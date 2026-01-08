ef05caa96a5af7a82cd0d18d4d6acd502243f2a1
Manjulmalla <mallamanjul4@gmail.com>
2026-01-09 00:44:23 +0545

    post crud operaion

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard, name='dashboard'),
+    #categories crud operation
     path('categories/',views.categories,name='categories'),
     path('categories/add/',views.add_category,name='add_category'),
     path('categories/edit/<int:pk>/',views.edit_category,name='edit_category'),
     path('categories/delete/<int:pk>/',views.delete_category,name='delete_category'),
 
+    # blog post crud
+    path('posts/',views.posts,name='posts'),
+    path('posts/add_posts/',views.add_posts,name='add_posts'),
+    path('posts/edit_post/<int:pk>/',views.edit_post,name='edit_post'),
+    path('posts/delete/<int:pk>/',views.delete_posts,name='delete_posts'),
 ]
